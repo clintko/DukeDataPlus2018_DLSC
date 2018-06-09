cf332851141ad9c6e14b4abc3ea1085acf7ec0d2
dzy <dzy@dzytekiMacBook-Pro.local>
2018-06-09 01:05:14 -0400

    I created machine learning folder running PCA tSNE and cluster in it

--- data_helper.py
+++ data_helper.py
@@ -2,43 +2,27 @@ import pandas as pd
 import numpy as np
 
 class data(object):
-
+    
     def __init__(self, filename):
         self.df = pd.read_table(filename)
-
+    
     # return a dictionary
     def readFile(self):
         dic = {}
-<<<<<<< HEAD
-        for column in self.geneList():
-            dic[column] = self.df[column].tolist()
-        print('dic')
-        print(dic)
-=======
         genes=self.geneList()
         for i in range(0,len(genes)):
             dic[genes[i]] = self.df.iloc[i].tolist()[1::]
->>>>>>> d2334e3f424ba67cf9c460abaf82565fcfb7e50b
         return dic
-
+    
     # return the list of cells
     def cellIndex(self):
         result = list(self.df.columns.values)
-        print('result[1::]')
-        print(result[1::])
         return result[1::]
-
-<<<<<<< HEAD
-    # return the list of cell index
-    def cellIndex(self):
-        print('list(self.df[GENE].values)')
-        print(list(self.df['GENE'].values))
-=======
+    
     # return the list of genes
     def geneList(self):
->>>>>>> d2334e3f424ba67cf9c460abaf82565fcfb7e50b
         return list(self.df['GENE'].values)
-
+    
     # return an numpy matrix of the values
     def getMatrix(self):
         dict=self.readFile()
@@ -54,4 +38,4 @@ if __name__ == "__main__":
     print(len(data.geneList()))
     print(data.geneList())
     print(data.cellIndex())
-    print(data.getMatrix().shape)
+print(data.getMatrix().shape)
